f83e4a12aafd6e7c75ccb2e4f4d99298187ee756
J. B. Saige <jbspublic@gmail.com>
2017-12-18 12:43:51 -0800

    This code matches what is in post 1. https://blog.gce.lupecode.com/2017/12/10/generating-random-passwords/

--- src/RandomCommand.php
+++ src/RandomCommand.php
@@ -5,7 +5,6 @@ namespace RndPwd;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class RandomCommand extends Command
@@ -16,24 +15,13 @@ class RandomCommand extends Command
         $this->setName("gen")
              ->setDescription("Generates a randomized password")
              ->addArgument('Password Length', InputArgument::REQUIRED, 'The length of the resulting password')
-             ->addArgument('Character Set', InputArgument::OPTIONAL, 'The set of characters to use')
-             ->addOption('u', null, InputOption::VALUE_NONE, 'Use untypable characters')
-             ->addOption('e', null, InputOption::VALUE_NONE, 'Use emoji characters')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $RP = new RandomPassword();
-        if($input->getArgument('Character Set')){
-            $pwd = $RP::generateRandomPassword($input->getArgument('Password Length'), $input->getArgument('Character Set'));
-        }elseif($input->getOption('u')){
-            $pwd = $RP::generateRandomPasswordUntypable($input->getArgument('Password Length'));
-        }elseif($input->getOption('e')){
-            $pwd = $RP::generateRandomPasswordEmoji($input->getArgument('Password Length'));
-        } else{
-            $pwd = $RP::generateRandomPassword($input->getArgument('Password Length'));
-        }
+        $RP  = new RandomPassword();
+        $pwd = $RP::generateRandomPassword($input->getArgument('Password Length'));
         $output->writeln($pwd);
     }
 }
